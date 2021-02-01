001b24d07f7fc0761076805c06ef674f571d73f4
fengzhu92 <zhufengq@gmail.com>
2021-01-31 21:11:06 -0800

    Updated model and uploaded test images to Samples

--- app.py
+++ app.py
@@ -1,25 +1,19 @@
-import os
 from flask import Flask, request, redirect, url_for, render_template
 from werkzeug.utils import secure_filename
+import tensorflow as tf
+import numpy as np
+from keras_preprocessing import image
+import os
 
+os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
+
+gestures = ['paper', 'rock', 'scissors']
 
 app = Flask(__name__)
 
-from keras.models import load_model
-from keras.backend import set_session
-from skimage.transform import resize
-import matplotlib.pyplot as plt
-import tensorflow as tf
-import numpy as np
-
 print("Loading model")
-global sess
-sess = tf.Session()
-set_session(sess)
-global model
-model = load_model('my_cifar10_model.h5')
-global graph
-graph = tf.get_default_graph()
+model = tf.keras.models.load_model('rps.h5')
+
 
 @app.route('/', methods=['GET', 'POST'])
 def main_page():
@@ -30,32 +24,24 @@ def main_page():
         return redirect(url_for('prediction', filename=filename))
     return render_template('index.html')
 
+
 @app.route('/prediction/<filename>')
 def prediction(filename):
-    #Step 1
-    my_image = plt.imread(os.path.join('uploads', filename))
-    #Step 2
-    my_image_re = resize(my_image, (32,32,3))
-    
-    #Step 3
-    with graph.as_default():
-      set_session(sess)
-      probabilities = model.predict(np.array( [my_image_re,] ))[0,:]
-      print(probabilities)
-      #Step 4
-      number_to_class = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship',
-'truck']
-      index = np.argsort(probabilities)
-      predictions = {
-        "class1":number_to_class[index[9]],
-        "class2":number_to_class[index[8]],
-        "class3":number_to_class[index[7]],
-        "prob1":probabilities[index[9]],
-        "prob2":probabilities[index[8]],
-        "prob3":probabilities[index[7]],
-      }
-    #Step 5
+    img = image.load_img(os.path.join('uploads', filename), target_size=(150, 150))
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    probabilities = model.predict([x])[0, :]
+    index = np.argsort(probabilities)
+    print(gestures[index[2]])
+    predictions = {
+        "class1": gestures[index[2]],
+        "class2": gestures[index[1]],
+        "class3": gestures[index[0]],
+        "prob1": probabilities[index[2]],
+        "prob2": probabilities[index[1]],
+        "prob3": probabilities[index[0]],
+    }
     return render_template('predict.html', predictions=predictions)
 
-app.run(host='0.0.0.0', port=80)
 
+app.run(host='0.0.0.0')
